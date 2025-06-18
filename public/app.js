64e209d6edbeea068b9b10c35f07ce19a1cd6d16
Benjamin Thomas <Benjamingathomas@gmail.com>
2025-06-18 07:30:50 -0400

    filters are set up to show correct elements

--- public/app.js
+++ public/app.js
@@ -3,14 +3,39 @@
 document.getElementById('filterForm').addEventListener('submit', async e => {
     e.preventDefault()
 
+    const form = e.target
+
+    const params = new URLSearchParams()
+    //using urlsearchparams will allow us to add the values and the keys to the url
+    
+    new FormData(form).forEach((value, key) => {
+        params.append(key, value)
+        //using for each and append this way will allow us to see the filters for each category with the use of & in the console. i.e.: level=intermediate&primaryMuscles=Biceps&category=Strength&category=Stretching
+    })
+
+        //console.log(params.toString())
+
     try {
-        const res = await fetch('/excersises')
+        const res = await fetch('/exercises?' + params.toString())
+        console.log('/exercises?' + params.toString())
         if(!res.ok) throw new Error(`HTTP ${res.status}`)
-        const excersices = await res.json()
-        console.log(excersices)
+        const exercises = await res.json() //this will format the response as JSON
+        
+        //list elements inside existing ul tag
+        const ul = document.getElementById('results')
+        if(exercises.length === 0) {
+            ul.innerHTML = '<li>No exercises found.</li>'
+        } else {
+            ul.innerHTML = exercises.map(exercise => {
+                return `<li>${exercise.name}</li>`
+            }).join('')
+        }
+
+        console.log(exercises.level)
     }
     catch (err) {
         console.log('Error fetching exercises:', err)
+        document.getElementById('results').innerHTML = '<li>The exercises decided to be lazy on the couch.</li>'
     }
 }
 
--- server.js
+++ server.js
@@ -13,11 +13,43 @@ const supabase = createClient(
 
 app.use(express.static('public'))
 
-app.get('/excersises', async (req, res) => {
-    console.log('Get /excersises hit')
+app.get('/exercises', async (req, res) => {
+
+    //this is an example of destructuring. destructuring is when you can extract values from an object based on its name. 
+    const {level = [], equipment = [], primaryMuscles = [], category = []} = req.info;
+
+    const levels = Array.isArray(level) ? level : [level]
+    const equipments = Array.isArray(equipment) ? equipment : [equipment]
+    const muscles = Array.isArray(primaryMuscles) ? primaryMuscles : [primaryMuscles]
+    const categories = Array.isArray(category) ? category : [category]
+
+    //console.log('Get /excersises hit')
     
-    let info = supabase.from('excersises')
-    .select('name')
+    let info = supabase.from('exercises').select('id, name, level, equipment, primaryMuscles, category')
+
+    if (levels.length > 0 && levels.length < 3) {
+        info = info.in('level', levels)
+    }
+
+    const wantsBodyOnly = equipments.includes('Body Only');
+    const wantsEquipmentReq = equipments.includes('Equipment Required');
+
+    if (wantsBodyOnly && !wantsEquipmentReq) {
+        info = info.eq('equipment', 'Body Only')
+    }
+
+    if (wantsEquipmentReq && !wantsBodyOnly) {
+        info = info.neq('equipment', 'Body Only')
+    }
+
+
+    if (muscles.length > 0 && muscles.length < 17) {
+        info = info.in('primaryMuscles', muscles)
+    }
+
+    if (categories.length > 0 && categories.length < 7) {
+        info = info.in('category', categories)
+    }
 
     const {data, error} = await info
     if (error) {
